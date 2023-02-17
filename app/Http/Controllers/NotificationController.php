e47f1fe39bde13eec50707dc0a5aa827817b0c1f
laper01 <yusufyahya0107@gmail.com>
2023-02-17 16:40:35 +0800

    add firebase credential

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Iluminate\Support\Facades\Http;
+
+class NotificationController extends Controller
+{
+    //
+
+    public function index()
+    {
+        return view('pushNotification');
+    }
+
+    public function sendNotification(Request $request)
+    {
+        $firebaseToken = User::whereNotNull('device_token')->pluck('device_token')->all();
+
+        $SERVER_API_KEY = env('FCM_SERVER_KEY');
+
+        $data = [
+            'registration_ids' => $firebaseToken,
+            'notification' => [
+                'title' => $request->title,
+                'body' => $request->body
+            ]
+        ];
+
+        // $dataString = json_encode($data);
+
+        $headers = [
+            'Authorization: key=' . $SERVER_API_KEY,
+            'Content-Type: application/json',
+        ];
+
+        $url = 'https://fcm.googleapis.com/fcm/send';
+
+        $reponse = Http::
+            withHeaders($headers)->
+            post($url, $data);
+
+            dd($reponse);
+    }
+
+
+}
